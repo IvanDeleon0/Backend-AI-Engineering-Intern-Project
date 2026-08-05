1a3e6abc891178715f1871bbd4545a124745b6be
Ivan Manimtim <ivancarldmanimtim@iskolarngbayan.pup.edu.ph>
2026-08-05 16:00:39 +0800

    Stage 4: full CRUD

--- New Assignment/main.py
+++ New Assignment/main.py
@@ -10,8 +10,10 @@ tasks = [{"id" : 1, "title": "Task 1", "done" : False},
          {"id" : 2, "title": "Task 2", "done" : True},
          {"id" : 3, "title": "Task 3", "done" : False}]
 
+#This class is used to validate the data sent in the request body when creating or updating a task.
 class Task(BaseModel):
     title: str
+    done: Optional[bool] = False
 
 @app.get("/")
 def root():
@@ -23,17 +25,22 @@ def root():
 def health_status():
     return {"status" : "ok"}
 
+#this endpoint returns all the tasks in the list
 @app.get("/tasks")
-def get_tasks():
+def display_tasks():
     return tasks
 
+
+# GET method is used to retrieve a resource, in this case a task.
 @app.get("/tasks/{id}")
-def get_task(id:int):
+def get_task_by_ID(id:int):
     for task in tasks:
         if task["id"] == id:
             return task
     raise HTTPException(status_code=404, detail={"error" : f"Task {id} not found"})
 
+
+# POST method is used to create a new resource, in this case a task.
 @app.post("/tasks", status_code=201)
 def add_task(task: Task):
     if not task.title or not task.title.strip():
@@ -47,3 +54,30 @@ def add_task(task: Task):
     }
     tasks.append(new_task)
     return new_task
+
+
+#PUT method is used to update a resource, in this case a task.
+@app.put("/tasks/{id}")
+def update_task(id: int, task: Task):
+    for t in tasks:
+        if t["id"] == id:
+            if not task.title or not task.title.strip():
+                raise HTTPException(status_code=400,
+                                    detail="Task title is invalid or empty")
+            t["title"] = task.title
+            return t
+    raise HTTPException(status_code=404, detail={"error" : f"Task {id} not found"})
+
+
+# status_code=204 means no content or confirmation sent back to the client 
+# but its a successful operation
+
+@app.delete("/tasks/{id}", status_code=204)
+def delete_task(id: int):
+    for t in tasks:
+        if t["id"] == id:
+            tasks.remove(t)
+            return
+    raise HTTPException(
+        status_code=404, 
+        detail={"error" : f"Task {id} not found"})
