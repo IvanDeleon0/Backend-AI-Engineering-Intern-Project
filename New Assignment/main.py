df14103b2a39c4f0811529b4b900ddc771dbda40
Ivan Manimtim <ivancarldmanimtim@iskolarngbayan.pup.edu.ph>
2026-08-05 15:16:21 +0800

    Stage 2

--- New Assignment/main.py
+++ New Assignment/main.py
@@ -6,9 +6,12 @@ from datetime import datetime, timezone
 
 app = FastAPI(title="Task CRUD API")   
 
+tasks = [{"id" : 1, "title": "Task 1", "done" : False},
+         {"id" : 2, "title": "Task 2", "done" : True},
+         {"id" : 3, "title": "Task 3", "done" : False}]
 
 @app.get("/")
-def gets():
+def root():
     return {"name" : "Task API",
             "version" : "1.0",
             "endpoints" : ["/tasks"]
@@ -16,3 +19,14 @@ def gets():
 @app.get("/health")
 def health_status():
     return {"status" : "ok"}
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+@app.get("/tasks/{id}")
+def get_task(id:int):
+    for task in tasks:
+        if task["id"] == id:
+            return task
+    raise HTTPException(status_code=404, detail={"error" : f"Task {id} not found"})
